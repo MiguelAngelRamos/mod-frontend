8332fb067b9531709ca090466375fa1a591d9d2b
Miguel Ramos <return.infox@gmail.com>
2023-03-22 09:54:21 -0300

    menu de navegacion funcional

--- 7.example/js/script.js
+++ 7.example/js/script.js
@@ -1,9 +1,14 @@
 function toggleMenu() {
-  let nav = document.querySelector("nav ul");
+  let nav = document.querySelector("nav ul"); // capturamos el ul
   nav.classList.toggle("mobile-nav");
   console.log(nav)
 }
 
+// var numero;
+// let numeropar;
+// const PI = 3.14;
+
+// ES6
 
 // Esta línea de código utiliza el método classList.toggle() para alternar la clase "mobile-nav" en el elemento nav. Si el elemento ya tiene la clase "mobile-nav", se la quita. Si no la tiene, se la agrega. En otras palabras, esta línea de código alterna la visibilidad del menú móvil.
 
