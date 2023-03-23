9bd1013e65fcb2955049f2758520b72fc149e549
Miguel Ramos <return.infox@gmail.com>
2023-03-23 08:30:55 -0300

    codigos actualizados

--- 7.example/js/script.js
+++ 7.example/js/script.js
@@ -4,6 +4,7 @@ function toggleMenu() {
   console.log(nav)
 }
 
+
 // var numero;
 // let numeropar;
 // const PI = 3.14;
@@ -11,5 +12,3 @@ function toggleMenu() {
 // ES6
 
 // Esta línea de código utiliza el método classList.toggle() para alternar la clase "mobile-nav" en el elemento nav. Si el elemento ya tiene la clase "mobile-nav", se la quita. Si no la tiene, se la agrega. En otras palabras, esta línea de código alterna la visibilidad del menú móvil.
-
-// En resumen, la función "toggleMenu()" selecciona el elemento de lista no ordenada dentro del elemento de navegación y alterna su clase "mobile-nav" para mostrar y ocultar el menú móvil.
