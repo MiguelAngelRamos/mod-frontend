ee3ce3de780a7e8b4c7b90d56df360a87c1f189c
Miguel Ramos <return.infox@gmail.com>
2023-03-27 09:59:51 -0300

    javascript

---
+++ javascript/teoria-javascript/js/varletconst/varletconst.js
@@ -0,0 +1,26 @@
+//* var 
+var nombre = "Pedro"; //* string
+var apellido = 'Gonzalez';
+//* Typescript
+// let moneda: number;
+// let pais: string; 
+console.log(nombre);
+nombre = 200;
+console.log(nombre);
+
+var nombre = "Javier";
+console.log(nombre);
+
+//* const
+const PI = 3.1415;
+console.log(PI);
+// PI = "Miguel";
+// console.log(PI);
+
+console.log(nombre)
+let precio = 2000;
+precio = "Hola Mundo";
+console.log(precio);
+//* no podemos declarar dos veces una variable
+//let precio = 200;
+//console.log(precio);
