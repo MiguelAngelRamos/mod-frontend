a668b2ba7377c0c75de7988d2d33abdd3f21b413
Miguel Ramos <return.infox@gmail.com>
2023-03-29 11:04:39 -0300

    teoria de javascript

---
+++ javascript/teoria-javascript/js/imprimir/1-ejercicio.js
@@ -0,0 +1,10 @@
+let nombre = 'Pedro';
+let edad = 18;
+let pais = 'Chile';
+let ocupacion = 'Estudiante';
+
+// console.log("El individuo: " + nombre + " tiene: " + edad + " de nacionalidad: " + pais + " y su ocupación es: " + ocupacion);
+
+//* alt + 96
+
+console.log(`El individuo ${nombre}, tiene: ${edad} de nacionalidad: ${pais} y su ocupación es: ${ocupacion}`);
