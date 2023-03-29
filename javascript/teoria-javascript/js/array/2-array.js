f7f23fe7481731089a92bae53979baf36303d29f
Miguel Ramos <return.infox@gmail.com>
2023-03-29 09:38:37 -0300

    array for

---
+++ javascript/teoria-javascript/js/array/2-array.js
@@ -0,0 +1,15 @@
+const productos = ["Televisor", "Mouse", "Notebook", "Reloj"];
+//*                    0      ,   1    ,     2     ,   3
+//* su longitud es de 4 son 4 elementos
+// console.log(productos);
+// console.log(productos[0]);
+// console.log(productos[1]);
+// console.log(productos[2]);
+// console.log(productos[3]);
+
+//* for ( for loop)
+
+//* i = i + 1; esto lo puedes escribir como i++
+for(let i=0; i<productos.length; i++) {
+  console.log(productos[i]);
+}
